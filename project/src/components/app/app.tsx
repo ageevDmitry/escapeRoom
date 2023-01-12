8b304a05b68a0120969b5e7bacbd609045b74b33
ageevDmitry <ageevdmitry2018@gmail.com>
2023-01-12 11:23:16 +0300

    Добавляет страницу Booking

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
 // import Main from '../../pages/main/main';
 // import Quest from '../../pages/quest/quest';
 // import Contacts from '../../pages/contacts/contacts';
-import Login from '../../pages/login/login';
+// import Login from '../../pages/login/login';
+import Booking from '../../pages/booking/booking';
 
 function App(): JSX.Element {
-  return <Login/>;
+  return <Booking/>;
 }
 
 export default App;
---
+++ project/src/pages/booking/booking.tsx
@@ -0,0 +1,37 @@
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import Map from '../../components/map/map';
+import FormBooking from '../../components/form-booking/form-booking';
+
+function Booking (): JSX.Element {
+
+  return (
+    <div className="wrapper">
+      <Header/>
+      <main className="page-content decorated-page">
+        <div className="decorated-page__decor" aria-hidden="true">
+          <picture>
+            <source type="image/webp" srcSet="img/content/maniac/maniac-bg-size-m.webp, img/content/maniac/maniac-bg-size-m@2x.webp 2x" /><img src="img/content/maniac/maniac-bg-size-m.jpg" srcSet="img/content/maniac/maniac-bg-size-m@2x.jpg 2x" alt="" width={1366} height={1959} />
+          </picture>
+        </div>
+        <div className="container container--size-s">
+          <div className="page-content__title-wrapper">
+            <h1 className="subtitle subtitle--size-l page-content__subtitle">Бронирование квеста
+            </h1>
+            <p className="title title--size-m title--uppercase page-content__title">Маньяк</p>
+          </div>
+          <div className="page-content__item">
+            <div className="booking-map">
+              <Map/>
+              <p className="booking-map__address">Вы&nbsp;выбрали: наб. реки Карповки&nbsp;5, лит&nbsp;П, м. Петроградская</p>
+            </div>
+          </div>
+          <FormBooking/>
+        </div>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Booking;
