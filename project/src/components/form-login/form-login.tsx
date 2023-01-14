c6da0c0a84058793eb08fb836d8f17dc083cdc82
ageevDmitry <ageevdmitry2018@gmail.com>
2023-01-14 17:34:25 +0300

    Добавляет заготовку валидации

--- project/src/components/form-login/form-login.tsx
+++ project/src/components/form-login/form-login.tsx
@@ -1,38 +1,42 @@
 import {useAppDispatch} from '../../hooks';
+import {useForm} from 'react-hook-form';
 import {loginAction} from '../../store/api-action';
+
 
 function FormLogin (): JSX.Element {
 
+  const {register, handleSubmit} = useForm();
   const dispatch = useAppDispatch();
+
+  const onSubmit = (evt) => {
+
+    dispatch(loginAction({
+      login: evt.email,
+      password: evt.password,
+    }
+    ));
+  };
 
   return (
     <div className="login__form">
-      <form className="login-form" action="https://echo.htmlacademy.ru/" method="post">
+      <form onSubmit={handleSubmit(onSubmit)} className="login-form" action="https://echo.htmlacademy.ru/" method="post">
         <div className="login-form__inner-wrapper">
           <h1 className="title title--size-s login-form__title">Вход</h1>
           <div className="login-form__inputs">
             <div className="custom-input login-form__input">
               <label className="custom-input__label" htmlFor="email">E&nbsp;–&nbsp;mail</label>
-              <input type="email" id="email" name="email" placeholder="Адрес электронной почты" required/>
+              <input type="email" id="email" placeholder="Адрес электронной почты" {...register('email', { required: true })}/>
             </div>
             <div className="custom-input login-form__input">
               <label className="custom-input__label" htmlFor="password">Пароль</label>
-              <input type="password" id="password" name="password" placeholder="Пароль" required/>
+              <input type="password" id="password" placeholder="Пароль" {...register('password', { required: true })}/>
             </div>
           </div>
-          <button className="btn btn--accent btn--general login-form__submit" type="submit"
-            onClick={() => {
-              dispatch(loginAction({
-                login: 'Oliver.conner@gmail.com',
-                password: 'password2',
-              }
-              ));
-            }}
-          >Войти
+          <button className="btn btn--accent btn--general login-form__submit" type="submit">Войти
           </button>
         </div>
         <label className="custom-checkbox login-form__checkbox">
-          <input type="checkbox" id="id-order-agreement" name="user-agreement" required />
+          <input type="checkbox" id="id-order-agreement" {...register('user-agreement', { required: true })}/>
           <span className="custom-checkbox__icon">
             <svg width={20} height={17} aria-hidden="true">
               <use xlinkHref="#icon-tick" />
