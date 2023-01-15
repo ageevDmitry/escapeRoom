330058f1d3b84ff74ac5faf7b06a916ae555e110
ageevDmitry <ageevdmitry2018@gmail.com>
2023-01-15 11:14:05 +0300

    Добавляет маркер на карту

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -1,5 +1,8 @@
 import {MapContainer, TileLayer, Marker} from 'react-leaflet';
 import './map.css';
+import L from 'leaflet';
+
+L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.5.0/dist/images/';
 
 function Map (): JSX.Element {
   return (
@@ -10,7 +13,7 @@ function Map (): JSX.Element {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[90.505, -0.09]}>
+          <Marker position={[51.505, -0.09]}>
           </Marker>
         </MapContainer>
       </div>
