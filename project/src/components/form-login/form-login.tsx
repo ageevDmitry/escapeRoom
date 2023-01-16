85beacead1688d07fc839e12c93f7abd92419e81
ageevDmitry <ageevdmitry2018@gmail.com>
2023-01-16 15:49:55 +0300

    Побеждает форму регистрации

--- project/src/components/form-login/form-login.tsx
+++ project/src/components/form-login/form-login.tsx
@@ -2,13 +2,12 @@ import {useAppDispatch} from '../../hooks';
 import {useForm} from 'react-hook-form';
 import {loginAction} from '../../store/api-action';
 
-
 function FormLogin (): JSX.Element {
 
 type Fields = {
   email: string;
   password: string;
-  'user-agreement': string;
+  'agreement': string;
 }
 
 const {register, handleSubmit, formState: { errors }} = useForm<Fields>();
@@ -32,28 +31,35 @@ return (
           <div className="custom-input login-form__input">
             <label className="custom-input__label" htmlFor="email">E&nbsp;–&nbsp;mail</label>
             <input type="email" id="email" placeholder="Адрес электронной почты"
-              {...register('email', { required: 'Укажите почту' })}
+              {...register('email', { required: true })}
               aria-invalid={errors.email ? 'true' : 'false'}
             />
-            {errors.email && <><br/><span role="alert">{errors.email?.message}</span></>}
+            {errors.email?.type === 'required' && <><br/><span role="alert">Укажите,пожалуйста, почту</span></>}
           </div>
           <div className="custom-input login-form__input">
             <label className="custom-input__label" htmlFor="password">Пароль</label>
-            <input type="password" id="password" placeholder="Пароль" {...register('password', { required: true })}/>
+            <input type="password" id="password" placeholder="Пароль" {...register('password', { required: true, pattern: /^(?=.*[0-9])(?=.*[A-Za-zА-Яа-яЁё])([A-Za-zА-Яа-яЁё0-9]+){3,}$/ })}
+              aria-invalid={errors.password ? 'true' : 'false'}
+            />
+            {errors.password?.type === 'required' && <><br/><span role="alert">Укажите, пожалуйста, пароль</span></>}
+            {errors.password?.type === 'pattern' && <><br/><span role="alert">Пароль должен состоять из более двух символов, а также содержать минимум одну букву и цифру</span></>}
           </div>
         </div>
         <button className="btn btn--accent btn--general login-form__submit" type="submit">Войти
         </button>
       </div>
       <label className="custom-checkbox login-form__checkbox">
-        <input type="checkbox" id="id-order-agreement" {...register('user-agreement', { required: true })}/>
+        <input type="checkbox" id="id-order-agreement" {...register('agreement', { required: true })}
+          aria-invalid={errors.agreement ? 'true' : 'false'}
+        />
+        {errors.agreement?.type === 'required' && <><br/><span role="alert">Ознакомтесь, пожалуйста, с правилами</span></>}
         <span className="custom-checkbox__icon">
           <svg width={20} height={17} aria-hidden="true">
             <use xlinkHref="#icon-tick" />
           </svg>
         </span>
         <span className="custom-checkbox__label">Я&nbsp;согласен с
-          <a className="link link--active-silver link--underlined" href="/">правилами обработки персональных данных</a>&nbsp;и пользовательским соглашением
+          <a className="link link--active-silver link--underlined" href="/"> правилами обработки персональных данных</a>&nbsp;и пользовательским соглашением
         </span>
       </label>
     </form>
